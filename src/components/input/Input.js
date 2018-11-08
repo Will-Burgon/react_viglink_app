686e6f92e8c3dcc3fd554137f1f91ce4fff80a30
Will Hutchinson <hutchiw1@gmail.com>
2018-11-08 19:46:24 +0000

    Update Input.js

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -4,8 +4,8 @@ import styles from './Input.module.css'
 const Input = (props)=>{
 return (
     <div className = {styles.input_wrapper}>
-        <input onChange={(event)=>props.getApi(event)} placeholder="Type your API Key here"></input>
-        <input onChange={(event)=>props.getSecret(event)} placeholder="Type your Secret Key here"></input>
+        <input onKeyPress={(event)=>props.getApi(event)} placeholder="Type your API Key here"></input>
+        <input onKeyPress={(event)=>props.getSecret(event)} placeholder="Type your Secret Key here"></input>
     </div>
 )
 }
